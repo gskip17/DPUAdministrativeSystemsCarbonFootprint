0370330a6623a8015c29d37141741ce96a441761
grantskipper <grantskipper_2017@depauw.edu>
2016-09-12 19:31:27 -0400

    Plane calculations now use DistanceMatrix API to calculate distance. Last thing to do is double check validation/error handling, and compose the JSON object to be POSTed to the back end.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -43,17 +43,18 @@ app.controller('MainController', ['$scope', function($scope){
       if(status == "OK"){
         directionsDisplay.setDirections(DirectionsResult);
       } else {
-        toastr.error('Error finding address, please make sure you are specific as possible with street addresses.');
+        toastr.error('Error finding address, please make sure you are as specific as possible with street addresses.');
       }
-      console.log(DirectionsResult);
+      //console.log(DirectionsResult);
 
-      totalDistance = DirectionsResult.routes[0].legs[0].distance.text;
-      $scope.totalMileage = DirectionsResult.routes[0].legs[0].distance.text;
+      totalDistance = Math.floor(DirectionsResult.routes[0].legs[0].distance.value * 0.000621371);
+      $scope.totalMileage = Math.floor(DirectionsResult.routes[0].legs[0].distance.value * 0.000621371);
 
-      console.log("distance = "+$scope.totalMileage);
-      console.log(DirectionsResult);
-      console.log(totalDistance);
-      console.log($scope.RoundTrip);
+      // If you are curious about the stuff we recieve, uncomment these lines of code.
+      //console.log("distance = "+$scope.totalMileage);
+      //console.log(DirectionsResult);
+      //console.log(totalDistance);
+      //console.log($scope.RoundTrip);
 
       // If it is a round trip, double the number.
       // Because google returns only a string for miles, we must
@@ -65,7 +66,7 @@ app.controller('MainController', ['$scope', function($scope){
 
         miles = Math.floor(miles);
 
-        $scope.totalMileage = miles + " mi";
+        $scope.totalMileage = miles;
         $scope.$apply();
       }
 
@@ -114,49 +115,76 @@ app.controller('MainController', ['$scope', function($scope){
   }
 
   $scope.getTrip = function(){
-    for(var leg in $scope.planeLegs){
-      var current = $scope.planeLegs[leg];
 
+    try{
+      if(validatePlaneTrip() == false){
+        return toastr.error('Please ensure you select all cities from the autocomplete drop down list.')
+      }
+    } catch (e){
+      return 0;
+      console.log(e);
+    }
+
+
+    totalDistance = 0;
+
+    for(var leg in $scope.planeLegs){
+      // Put our current leg in a readable format.
+      var current = $scope.planeLegs[leg];
+      // Create LatLng objects from geolocation metadata provided by autocomplete.
       var startLat = current.StartDetails.geometry.location.lat();
       var startLng = current.StartDetails.geometry.location.lng();
-      var start = new google.maps.LatLng({startLat, startLng});
-      console.log(startLat);
-      console.log(startLng);
-      console.log(start);
+      var start = new google.maps.LatLng(startLat, startLng);
 
       var destLat = current.EndDetails.geometry.location.lat();
       var destLng = current.EndDetails.geometry.location.lng();
-      var dest = new google.maps.LatLng({destLat, destLng});
+      var dest = new google.maps.LatLng(destLat, destLng);
 
-      console.log(start);
-      console.log(dest);
-      var distance = google.maps.geometry.spherical.computeDistanceBetween (start, dest);
+      // Should probably put the converting float into a constant. For those confused about the 0.000621371,
+      // this float converts the distance given by google (in meters) to miles.
+      var distance = google.maps.geometry.spherical.computeDistanceBetween(start, dest) * 0.000621371;
       console.log("Computed distance is " + distance);
+
+      // Save the metadata for miles in the local object (in case we need specific data later in the JSON submission).
+      current.Miles = distance;
+      // Compound the total before moving on.
+      totalDistance += distance;
     }
 
+    // Round the miles to the nearest whole number (way too many decimals).
+    totalDistance = Math.floor(totalDistance);
+    console.log("Total distance traveled is " + totalDistance);
+    $scope.totalMileage = totalDistance;
   };
 
-  function getDistance(start, dest){
 
-    var service = new google.maps.DistanceMatrixService;
-        service.getDistanceMatrix({
-          origins: [start],
-          destinations: [dest],
-          travelMode: 'DRIVING',
-          unitSystem: google.maps.UnitSystem.METRIC,
-          avoidHighways: false,
-          avoidTolls: false
-        }, function(response, status) {
-          if (status !== 'OK') {
-            alert('Error was: ' + status);
-          }
+  // Helper function. We just quickly loop through the legs array to ensure
+  // that a user picked a valid Google location from the autocomplete dropdown.
+  // Otherwise we would have no coordinates to do the DistanceMatrix stuff.
+  function validatePlaneTrip() {
 
-          console.log(response);
+    for (var leg in $scope.planeLegs){
+      var current = $scope.planeLegs[leg];
+      // if StartDetails or EndDetails is false (no input), then the input was not filled
+      // in from the autocomplete dropdown.
+      if(!current.StartDetails || !current.EndDetails){
+        return false;
+      }
+    }
+    // If we get here, this means the form has been filled out correctly.
+    return true;
 
-        });
   }
 
+  // Checks the form for a value in the main data inputs. (Not a complete validation).
+  function validateBody(){
+    if(!$scope.NumStudents || !$scope.NumFaculty || !$scope.DateOfTravel){
+      return false;
+    }
+    return true;
+  }
 
+  // Initializes the pretty map we see.
   function initMap(){
 
     directionsDisplay = new google.maps.DirectionsRenderer();
@@ -166,6 +194,10 @@ app.controller('MainController', ['$scope', function($scope){
       zoom:14,
       center: depauw
     }
+
+    // WIERDNESS, you MUST use getElementById, and NOT a jQuery selector to
+    // reference the div containing the map. I need to check jQuery source and
+    // see if I can find a reason for this..
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
     directionsDisplay.setMap(map);
 
